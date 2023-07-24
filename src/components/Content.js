cdb276f210f526be6198e3182c487d5cbfedbe61
kiatklai <likhitthaworn.k@gmail.com>
2023-07-24 21:36:37 +0900

    component作成

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import light from '../image/light.svg'
+
+const Content=()=>{
+  return(
+    <main>
+      <div>
+        <h1>Ooh_Gotto Official</h1>
+        <p>DarkMode Workshop</p>
+      </div>
+      <img src={light} alt='Logo'/>
+    </main>
+  )
+}
+
+export default Content
