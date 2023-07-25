1e4825b50a79d99545b86c44a74ed4c56c2e5c6b
kiatklai <likhitthaworn.k@gmail.com>
2023-07-25 14:02:43 +0900

    react switch導入

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+import Switch from 'react-switch'
+
 const Title =()=>{
+  const [theme,setTheme] = useState(false)
+  const toggleSwitch=(checked)=>{
+    setTheme(checked)
+  }
   return(
     <header>
       <span>Mode</span>
+      <Switch
+        onChange={toggleSwitch}
+        checked = {theme}/>
     </header>
   )
 }
